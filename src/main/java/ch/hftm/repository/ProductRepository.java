000e4cf6ff6739162eb3534d066edbb78f6c3844
Felice Pettinaro <felice@pettinaro.xyz>
2024-03-20 22:47:10 +0100

    Refactor ProductRepository and ProductService

--- src/main/java/ch/hftm/repository/ProductRepository.java
+++ src/main/java/ch/hftm/repository/ProductRepository.java
@@ -2,6 +2,10 @@ package ch.hftm.repository;
 
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.HashMap;
+import java.util.List;
+
 import org.bson.types.ObjectId;
 
 import ch.hftm.model.Product;
@@ -12,5 +16,13 @@ public class ProductRepository implements PanacheMongoRepository<Product>, IRepo
 
     public boolean isPersisted(ObjectId id) {
         return findById(id) != null;
+    }
+
+    public List<Product> findAllProducts(int page, int pageSize) {
+        return find("from Product", new HashMap<>(), page, pageSize).list();
+    }
+
+    public Long countAll() {
+        return count("from Product");
     }
 }
--- src/main/java/ch/hftm/services/ProductService.java
+++ src/main/java/ch/hftm/services/ProductService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.bson.types.ObjectId;
 import org.jboss.logging.Logger;
 
+import ch.hftm.model.Order;
 import ch.hftm.model.Product;
 import ch.hftm.repository.ProductRepository;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -18,35 +19,98 @@ public class ProductService {
     public record ItemRes(String id, boolean isValid) {}
 
     @Inject
-    ProductRepository invItemRepo;
+    ProductRepository productRepo;
 
     private static final Logger LOG = Logger.getLogger(ProductService.class);
 
+    public Product createProduct(Product product) {
+        LOG.info("Creating new item: " + product.getName() + " | ID: " + product.id);
+        if (product.getName() == null || product.getName().isEmpty()) {
+            throw new IllegalArgumentException("Name is required");
+        }
+        if (productRepo.isPersisted(product.id)) {
+            throw new IllegalArgumentException("Item already exists");
+        } 
+        productRepo.persist(product);
+        if (productRepo.isPersisted(product.id)){
+            LOG.info("Sending item for validation: " + product.id);
+            return productRepo.findById(product.id);
+        } 
+        return null;
+    }
+
+    public List<Product> getAllProducts() {
+        LOG.info(LOG.getName());
+        return productRepo.findAll().list();
+    }
+
+    public Product getProductById(ObjectId id) {
+        return productRepo.findById(id);
+    }
+
+    public Product updateProduct(Product product) {
+        LOG.info("Updating item: " + product.id);
+        if (product.id == null) {
+            throw new IllegalArgumentException("id is required");
+        }
+        if (productRepo.isPersisted(product.id)) {
+            productRepo.update(product);
+            return productRepo.findById(product.id);
+        }
+        return null;
+    }
+
+    public Long countAll() {
+        return productRepo.countAll();
+    }
+
+    public List<Product> getAllProducts(int page, int pageSize) {
+        return productRepo.findAllProducts(page, pageSize);
+    }
+
+    public void deleteProduct(ObjectId id) {
+        LOG.info("Deleting item: " + id);
+        Product product = productRepo.findById(id);
+        if (product != null) {
+            productRepo.delete(product);
+            LOG.info("Deleted item: " + id);
+        }
+    }
+
+    public void deleteAllProducts() {
+        LOG.info("Deleting all products");
+        productRepo.deleteAll();
+        LOG.info("All products deleted");
+    }
+
+
+    //TODO: Validate this methods 
+
     public List<Product> listAll() {
         LOG.info(LOG.getName());
-        return invItemRepo.listAll();
+        return productRepo.listAll();
     }
 
     public void persist(Product item) {
         LOG.info("Persisting item: " + item.id);
-        invItemRepo.persist(item);
+        productRepo.persist(item);
     }
 
     public boolean isPersistend(ObjectId id) {
-        return invItemRepo.isPersisted(id);
+        return productRepo.isPersisted(id);
     }
 
     public Product find(ObjectId id) {
-        return invItemRepo.findById(id);
+        return productRepo.findById(id);
     }
 
     public boolean delete(Product item) {
-        return invItemRepo.deleteById(item.id);
+        return productRepo.deleteById(item.id);
     }
 
     public boolean delete(boolean all) {
         try {
-            invItemRepo.deleteAll();
+            productRepo.deleteAll();
             return true;
         } catch (Exception e) {
             LOG.error("Error deleting all items: " + e.getMessage());
@@ -57,7 +121,7 @@ public class ProductService {
     //TODO: Refactor this method to use the ItemReq record
     public void sendItemForValidation(ObjectId itemid) {
         LOG.info("Sending item for validation: " + itemid);
-        Product item = invItemRepo.findById(itemid);
+        Product item = productRepo.findById(itemid);
         LOG.info("Payload Item: " + item.id);
         if (item != null) {
             LOG.info("Sended item for validation: " + item.id);
@@ -69,12 +133,12 @@ public class ProductService {
     public void updatedItemValidationStatus(ItemRes message) {
         LOG.info("Received validation result: " + message);
         ObjectId objectId = new ObjectId(message.id());
-        Product item = invItemRepo.findById(objectId);
+        Product item = productRepo.findById(objectId);
         LOG.info("Payload Item: " + objectId);
         LOG.info("Payload Item: " + message.isValid());
         if (item != null) {
             LOG.info("Persisting item: " + item.id);
-            invItemRepo.update(item);
+            productRepo.update(item);
             LOG.info("Persisted item: " + item.id);
         }
         LOG.info("Payload Item: ");
